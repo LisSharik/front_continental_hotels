7a487392a5f548f9037273906c197b563313f5c5
SantiagoProgramador <andresx1277@gmail.com>
2024-05-10 13:17:15 -0500

    starting room details :D

---
+++ src/components/room_details.tsx
@@ -0,0 +1,24 @@
+
+
+
+function room_details(roomId:string) {
+  //getInfo("/room");
+
+  return (<>
+    `Room ${roomId} is now available for booking!`
+    </>
+  );
+}
+
+
+export default room_details;
+
+
+//function getInfo(endPoint: string){
+//  const url = http://localhost:8080/api/v1/{endPoint};
+//  
+//  const response = wdawdawdawdM;
+//
+//
+//  return response;
+//}
