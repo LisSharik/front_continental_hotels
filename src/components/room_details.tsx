58dda7dbdbbaac5f3c5ad72e6a1a4140f148492a
SantiagoProgramador <andresx1277@gmail.com>
2024-05-14 15:14:44 -0500

    Working on the slider for the type of room

--- src/components/room_details.tsx
+++ src/components/room_details.tsx
@@ -1,11 +1,29 @@
+import { Key, useRef } from "react";
+import {roomImages} from "../data/roomImages.json";
 
 
 
-function room_details(roomId:string) {
+function room_details() {
   //getInfo("/room");
 
+  
+
   return (<>
-    `Room ${roomId} is now available for booking!`
+      <div>
+        <div>
+          <button>Prev</button>
+          <div> 
+            <ul>
+              {roomImages.map((room: { id: Key | null | undefined; url: string | undefined; }) => {
+                <li key={room.id}>
+                  <img src={room.url} alt="room" />
+                </li>
+              })}
+            </ul>
+          </div>
+          <button>Next</button>
+        </div>
+      </div>
     </>
   );
 }
