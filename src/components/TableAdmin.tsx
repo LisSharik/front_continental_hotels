f89525c46403c3f46373d3d71674b275a41a0a79
lisSharik <lissharik@gmail.com>
2024-05-17 15:57:07 -0500

    Styles table Admin

--- src/components/TableAdmin.tsx
+++ src/components/TableAdmin.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {Button} from "@nextui-org/react";
+import {Input} from "@nextui-org/react";
+
 
 interface TableAdminProps {
     nameTable: string;
@@ -31,18 +33,29 @@ const TableAdmin: React.FC<TableAdminProps> = ({ nameTable, iconTable, data }) =
 
     return (
         <div className="w-[80vw] h-full flex flex-col justify-start items-center p-4 table-admin">
-            <div className="w-11/12 h-[10%] bg-CWhite flex justify-start items-center mb-4">
+            <div className="w-11/12 h-[10%] bg-CWhite flex justify-between items-center mb-16 px-[60px] rounded-lg shadow-md ">
                 <div className="flex justify-start items-center  gap-2 title">
                     <i className={`bx ${iconTable} icon-table`}></i>
                     <h2 className="text-subtitulos font-Lato  font-bold">{nameTable}</h2>
                 </div>
                 <Button className="bg-[#1c9738] text-CWhite btn-add">
-                    <i className='bx bxs-plus-circle '></i>
+                    <i className='bx bxs-plus-circle  '></i>
                     Agregar  
                 </Button>
             </div>
+
+            <Input
+                type="email"
+                label="Email"
+                placeholder="you@example.com"
+                labelPlacement="outside"
+                className="w"
+                startContent={
+                    <i className='bx bxs-filter-alt' ></i>
+                }
+            />
             {data.length > 0 ? (
-                <table className="table-auto w-full">
+                <table className="table-auto w-11/12 bg-CWhite rounded-lg shadow-md ">
                     <thead>
                         {renderTableHeader()}
                     </thead>
