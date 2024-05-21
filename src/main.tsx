e9eae3e7492f8af4502f248a4ce3f1c1e961dd5a
lisSharik <lissharik@gmail.com>
2024-05-21 06:42:42 -0500

    Organizando carpetas

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
 import App from './App'
 // import './index.css'
-import './Styles/index.css'
+import './index.css'
 
 
 
