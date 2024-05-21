f1944c195d83acabd08bfc07118a02931ddaa286
Juan Atehortua <118418932+JuanCamilo97-stack@users.noreply.github.com>
2024-05-20 20:14:23 -0500

    Profile.tsx folder added with user data form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-// src/App.tsx
 import React from "react";
-import AdminPage from "./pages/AdminPage";
-import Contact from "./components/Contact";
-import BookingForm from "./components/BookingForm";
-import BannerIndex from "./components/BannerIndex";
-import LuxuryRooms from "./components/LuxuryRooms";
+import Profile from "./components/Profile"; 
 
 const App: React.FC = () => {
     return (
-      <div className="w-screen h-screen flex items-center justify-start flex-col ">
-        <AdminPage/>
-        
-      </div>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Profile />
+    </div>
     );
 }
 
